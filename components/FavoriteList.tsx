ef4572e6b9f886c677a86acd8452599c9bc64ca5
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-05 14:40:42 +0700

    Check empty favorite list

--- components/FavoriteList.tsx
+++ components/FavoriteList.tsx
@@ -1,16 +1,22 @@
 "use client";
 import { useAddFavorite } from "@/utils/store";
 import FavoriteItem from "./FavoriteItem";
-import { Grid } from "@radix-ui/themes";
+import CarsNotFound from "./CarsNotFound";
 
 const FavoriteList = () => {
   const favoriteList = useAddFavorite((state) => state.favoriteList);
   return (
-    <section className="grid grid-cols-1 gap-4">
-      {favoriteList.map((item, index) => (
-        <FavoriteItem car={item} key={index} />
-      ))}
-    </section>
+    <>
+      {favoriteList.length > 0 ? (
+        <section className="grid grid-cols-1 gap-4">
+          {favoriteList.map((item, index) => (
+            <FavoriteItem car={item} key={index} />
+          ))}
+        </section>
+      ) : (
+        <CarsNotFound />
+      )}
+    </>
   );
 };
 
