6e1139c18ee4640fe2d0880cfe5634eab23023fb
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-06 18:04:20 +0700

    debug

--- services/car.service.ts
+++ services/car.service.ts
@@ -4,7 +4,7 @@ import { ICar } from "@/types/types";
 class CarService {
   async getAll() {
     return axios.get<ICar[]>(
-      "https://6454dae6a74f994b334ad4fb.mockapi.io/carShop"
+      `https://6454dae6a74f994b334ad4fb.mockapi.io/carShop`
     );
   }
 
