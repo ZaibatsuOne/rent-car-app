54610895baffc658cc568aad624c3642913b35ea
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-06 17:44:11 +0700

    Add metadata

--- app/(catalogue)/layout.tsx
+++ app/(catalogue)/layout.tsx
@@ -1,9 +1,14 @@
 import AsideNavbar from "@/components/dashboard/AsideNavbar";
+import { Metadata } from "next";
 import { ReactNode } from "react";
 
 interface Layout {
   children: ReactNode;
 }
+export const metadata: Metadata = {
+  title: "MORENT - Catalogue",
+};
+
 const layout = ({ children }: Layout) => {
   return (
     <main className="flex ">
--- app/billing/[id]/page.tsx
+++ app/billing/[id]/page.tsx
@@ -1,9 +1,9 @@
 import BillingInfo from "@/components/Form/BillingInfoForm";
 import ConfiramtionForm from "@/components/Form/ConfiramtionForm";
-import PaymentMethodForm from "@/components/Form/PaymentMethodForm";
 import RentalInfoForm from "@/components/Form/RentalInfoForm";
 import RentalSummary from "@/components/RentalSummary";
 import carService from "@/services/car.service";
+import { Metadata } from "next";
 
 export const getOneCar = async (id: number) => {
   const data = await carService.getById(id);
@@ -13,6 +13,15 @@ export const getOneCar = async (id: number) => {
 interface Props {
   params: { id: number };
 }
+
+export const generateMetadata = async (props: Props): Promise<Metadata> => {
+  const { params } = props;
+  const car = await getOneCar(params.id);
+  return {
+    title: "Billing - " + car.name,
+  };
+};
+
 const BillingPage = async ({ params: { id } }: Props) => {
   const car = await getOneCar(id);
   return (
