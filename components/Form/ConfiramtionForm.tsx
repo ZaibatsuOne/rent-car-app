d10c399c6cb5e6dc55a0994e73f913d162150c98
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-07 12:46:04 +0700

    fix checkbox in billing page

--- components/Form/ConfiramtionForm.tsx
+++ components/Form/ConfiramtionForm.tsx
@@ -1,66 +1,38 @@
 "use client";
-import { ShieldCheck } from "lucide-react";
 import BillingCard from "../BillingCard";
+import { ShieldCheck } from "lucide-react";
 import { Button } from "../ui/button";
-import { Checkbox } from "../ui/checkbox";
 import { Label } from "../ui/label";
-import { useFormStore } from "@/utils/store";
-import { FormState, IFormState } from "@/types/types";
+import { useState } from "react";
 
 const ConfiramtionForm = () => {
-  const { formData, setFormData } = useFormStore() as FormState;
+  //Проверка чекбокса почты
+  const [emailCheck, setEmailCheck] = useState(false);
+  //Проверка чекбокса Terms
+  const [termsCheck, setTermsCheck] = useState(false);
 
-  const handleEmailCheckboxChange = (isChecked: boolean) => {
-    setFormData({
-      ...formData,
-      agreeEmail: isChecked,
-    });
-  };
-
-  const handleTermsCheckboxChange = () => {
-    setFormData({
-      ...formData,
-      agreeTerms: !formData.agreeTerms,
-    });
-  };
-
-  const handleRentNowClick = () => {
-    console.log("FormData:", formData);
-    console.log("Email Checked:", formData.agreeEmail);
-    console.log("Terms Checked:", formData.agreeTerms);
-  };
-
-  const isFormValid = () => {
-    const areFieldsValid = Object.values(formData).every(
-      (value) => value !== ""
-    );
-    const isEmailChecked = formData.agreeEmail;
-    const isTermsChecked = formData.agreeTerms;
-
-    return areFieldsValid && isEmailChecked && isTermsChecked;
-  };
-  console.log(isFormValid);
   return (
     <BillingCard step={3}>
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-6">
           <div className="flex items-center space-x-2 bg-[#F6F7F9] py-4 px-8 rounded-xl">
-            <Checkbox
-              id="email"
-              defaultChecked={formData.agreeEmail}
-              onCheckedChange={handleEmailCheckboxChange}
+            <input
+              type="checkbox"
+              className="w-4 h-4"
+              value={emailCheck}
+              onChange={(e) => setEmailCheck(e.target.checked)}
             />
-
             <Label htmlFor="email" className="text-xs lg:text-base">
               I agree with sending an Marketing and newsletter emails. No spam,
               promissed!
             </Label>
           </div>
           <div className="flex items-center space-x-2 bg-[#F6F7F9] py-4 px-8 rounded-xl">
-            <Checkbox
-              id="terms"
-              onChange={handleTermsCheckboxChange}
-              defaultChecked={formData.agreeTerms}
+            <input
+              type="checkbox"
+              className="w-4 h-4"
+              value={termsCheck}
+              onChange={(e) => setTermsCheck(e.target.checked)}
             />
             <Label htmlFor="terms" className="text-xs lg:text-base">
               I agree with our terms and conditions and privacy policy.
@@ -68,9 +40,7 @@ const ConfiramtionForm = () => {
           </div>
         </div>
         <div>
-          <Button disabled={!isFormValid()} onClick={handleRentNowClick}>
-            Rent Now
-          </Button>
+          <Button onClick={() => console.log(termsCheck)}>Rent Now</Button>
         </div>
         <div className="flex flex-col gap-4">
           <div className="flex items-center gap-2">
